e07517015836bec05596f88c6c92d59affa57a45
jandedeuraerder-howest <jan.de.deurwaerder@howest.be>
2020-10-14 13:17:05 +0200

    Add project files.

---
+++ Prb.ArraysRandomGetallen.WPF/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Prb.ArraysRandomGetallen.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        Random rnd = new Random();
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+        private void btnVersion1Add_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void btnVersion1Multiply_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnVersion2Add_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnVersion2Multiply_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+
+    }
+}
